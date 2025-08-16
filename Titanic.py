bd8f857dabb7e1ec96ed3b8c53a32b222a7b7630
Miguel Manalili <manalili.mig@gmail.com>
2025-08-16 19:16:31 +0100

    Finally got it running

--- Titanic.py
+++ Titanic.py
@@ -6,3 +6,9 @@ train = pd.read_csv(r"C:\Users\manal\Desktop\Titanic ML Submission\titanic\train
 test = pd.read_csv(r"C:\Users\manal\Desktop\Titanic ML Submission\titanic\test.csv")
 
 train_df = pd.DataFrame(train)
+
+test_df = pd.DataFrame(test)
+
+train_df.head(10)
+
+test_df.head(10)
