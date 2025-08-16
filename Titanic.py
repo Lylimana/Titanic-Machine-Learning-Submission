b7f36a95972d06792f9b5c1c843d33809e8dd7bc
Miguel Manalili <manalili.mig@gmail.com>
2025-08-16 19:07:10 +0100

    Importing Data

--- Titanic.py
+++ Titanic.py
@@ -4,3 +4,5 @@ import matplotlib.pyplot as plt
 
 train = pd.read_csv(r"C:\Users\manal\Desktop\Titanic ML Submission\titanic\train.csv")
 test = pd.read_csv(r"C:\Users\manal\Desktop\Titanic ML Submission\titanic\test.csv")
+
+train_df = pd.DataFrame(train)
