d416cf615bf83452c996bb126082797b368ac1bd
Miguel Manalili <manalili.mig@gmail.com>
2025-08-16 18:27:34 +0100

    Data

---
+++ Titanic.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+train = pd.read_csv(r"C:\Users\manal\Desktop\Titanic ML Submission\titanic\train.csv")
+test = pd.read_csv(r"C:\Users\manal\Desktop\Titanic ML Submission\titanic\test.csv")
